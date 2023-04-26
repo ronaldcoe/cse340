2df1463351c50909b9fb57e80e93bcaab1fd5483
Ronald <ronaldcoello85@gmail.com>
2023-04-26 14:35:56 -0600

    more changes

--- server.js
+++ server.js
@@ -6,13 +6,22 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
